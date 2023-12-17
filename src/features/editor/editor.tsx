11ca01d80a0fe9a34454440c99322b1622cd513c
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2023-12-17 12:05:56 +0530

    very basic socket setup

--- src/features/editor/editor.tsx
+++ src/features/editor/editor.tsx
@@ -1,5 +1,7 @@
 import useWindowResize from "@/hooks/useWindowResize";
 import NonInteractiveHeader from "@/layouts/Header";
+import { ObjectBaseOptions } from "@/utils/baseObjectOptions";
+import { socket } from "@/socket";
 
 import { fabric } from "fabric";
 import { useEffect, useRef, useState } from "react";
@@ -22,6 +24,45 @@ function Editor() {
     fabricInst?.setWidth(windowWidth);
     fabricInst?.setHeight(windowHeight);
   }, [windowHeight, windowWidth]);
+
+  useEffect(() => {
+    if (fabricInst) {
+      const rect = new fabric.Rect({
+        ...ObjectBaseOptions,
+        top: 0,
+        left: 0,
+        width: 100,
+        height: 100,
+      });
+
+      fabricInst.add(rect);
+    }
+  }, [fabricInst]);
+
+  useEffect(() => {
+    if (fabricInst) {
+      fabricInst?.on("object:moving", (e) => {
+        socket.emit("moving", {
+          left: e.target?.left,
+          top: e.target?.top,
+        });
+      });
+    }
+  }, [fabricInst]);
+
+  useEffect(() => {
+    if (fabricInst) {
+      socket.on("moving", (val) => {
+        fabricInst._objects[0].set({
+          left: val.left,
+          top: val.top,
+        });
+        fabricInst._objects[0].setCoords();
+
+        fabricInst.renderAll();
+      });
+    }
+  }, [fabricInst]);
 
   return (
     <>
