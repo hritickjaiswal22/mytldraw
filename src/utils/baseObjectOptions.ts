3fe3da4c26b31349eada81b3144a1cc9d710b579
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2023-12-17 10:35:25 +0530

    set base shape options

---
+++ src/utils/baseObjectOptions.ts
@@ -0,0 +1,15 @@
+import { fabric } from "fabric";
+
+const ObjectBaseOptions: fabric.IObjectOptions = {
+  fill: "transparent",
+  stroke: "black",
+  strokeWidth: 2,
+  padding: 4,
+  cornerStyle: "circle",
+  cornerSize: 8,
+  cornerStrokeColor: "#5b57d1",
+  // cornerColor: "orange",
+  borderColor: "#5b57d1",
+};
+
+export { ObjectBaseOptions };
