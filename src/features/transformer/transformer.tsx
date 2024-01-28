a5dad60f48fb598290dec8b45099ebf4ca3739dd
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2024-01-28 09:01:12 +0530

    pan option

--- src/features/transformer/transformer.tsx
+++ src/features/transformer/transformer.tsx
@@ -6,6 +6,9 @@ import { IEvent } from "fabric/fabric-impl";
 
 function Transformer() {
   const isAltPressed = useRef(false);
+  const isPanning = useRef(false);
+  const lastPosX = useRef(0);
+  const lastPosY = useRef(0);
   const { fabricInst } = useContext(FabricCanvasContext);
 
   function setAlt(e: KeyboardEvent) {
@@ -15,13 +18,6 @@ function Transformer() {
   function resetAlt() {
     isAltPressed.current = false;
   }
-
-  // function mouseMoveHandler(e: IEvent<MouseEvent>) {
-  //   const P = new fabric.Point(e.pointer?.x, e.pointer?.y);
-  //   const newP = fabric.util.transformPoint(P, fabricInst?.viewportTransform);
-
-  //   console.log(newP);
-  // }
 
   function zoom(opt: IEvent<WheelEvent>) {
     if (isAltPressed.current) {
@@ -37,16 +33,56 @@ function Transformer() {
     }
   }
 
+  function setPan({ e }: IEvent<MouseEvent>) {
+    if (isAltPressed.current && fabricInst) {
+      isPanning.current = true;
+      lastPosX.current = e.clientX;
+      lastPosY.current = e.clientY;
+      fabricInst.selection = false;
+    }
+  }
+
+  function pan({ e }: IEvent<MouseEvent>) {
+    if (isPanning.current && fabricInst) {
+      const vpt = fabricInst.viewportTransform;
+      if (vpt) {
+        vpt[4] += e.clientX - lastPosX.current;
+        vpt[5] += e.clientY - lastPosY.current;
+        fabricInst.requestRenderAll();
+        lastPosX.current = e.clientX;
+        lastPosY.current = e.clientY;
+
+        fabricInst.setCursor("grabbing");
+      }
+    }
+  }
+
+  function unsetPan() {
+    if (fabricInst) {
+      fabricInst.setViewportTransform(fabricInst.viewportTransform as any);
+      isPanning.current = false;
+      fabricInst.selection = true;
+
+      fabricInst.setCursor("auto");
+    }
+  }
+
   useEffect(() => {
     document.addEventListener("keydown", setAlt);
     document.addEventListener("keyup", resetAlt);
     // fabricInst?.on("mouse:move", mouseMoveHandler);
     fabricInst?.on("mouse:wheel", zoom);
+    fabricInst?.on("mouse:down", setPan);
+    fabricInst?.on("mouse:move", pan);
+    fabricInst?.on("mouse:up", unsetPan);
 
     return () => {
       document.removeEventListener("keydown", setAlt);
       document.removeEventListener("keyup", resetAlt);
       fabricInst?.off("mouse:wheel", zoom as any);
+      fabricInst?.off("mouse:down", setPan as any);
+      fabricInst?.off("mouse:move", pan as any);
+      fabricInst?.off("mouse:up", unsetPan);
     };
   }, []);
 
